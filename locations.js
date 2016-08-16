21ca6e85affb18908ec27ceb69d8a96f01b21293
Jannis R <mail@jannisr.de>
2016-08-16 03:29:58 +0200

    /locations: HTTP 400 on missing query

--- locations.js
+++ locations.js
@@ -4,7 +4,18 @@ const db = require('db-hafas')
 
 
 
+const err400 = (msg) => {
+	const e = new Error(msg)
+	e.statusCode = 400
+	return e
+}
+
+
+
 module.exports = (req, res, next) => {
+	if ('string' !== typeof req.query.q)
+		return next(err400('missing q parameter.'))
+
 	db.locations(req.query.q)
 	.then((data) => {res.json(data)}, next)
 	.catch(next)
